21f77528ee8822cc229048b7f0ad70f346dec2f4
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2022-02-08 12:11:41 +0400

    feat: add setTransleteWord

--- src/pages/Sprint/SprintGame.jsx
+++ src/pages/Sprint/SprintGame.jsx
@@ -10,26 +10,45 @@ function getRandomNum(min, max) {
   return Math.floor(rand);
 }
 
+function isRight() {
+  return Math.random() > 0.5;
+}
+
 function SprintGame({ level }) {
   const { isAuth } = useContext(AuthContext);
-  const [words, setWords] = useState([]);
   const [wordIndex, setWordIndex] = useState(0);
-  //   const writeWords = (words) => {
-  //     console.log(words);
-  //   };
+  const [translate, setTranslate] = useState('');
+  function setTranslateWord() {
+    console.log('setTranslate');
+    if (isRight()) {
+      setTranslate(words[wordIndex]?.wordTranslate);
+    } else {
+      let tmpIndex = 0;
+      if (words?.length) {
+        tmpIndex = getRandomNum(0, words?.length - 1);
+        while (tmpIndex === wordIndex) {
+          tmpIndex = getRandomNum(0, words?.length - 1);
+        }
+      }
+
+      setTranslate(words[tmpIndex]?.wordTranslate);
+    }
+    console.log(translate, words[wordIndex]?.wordTranslate);
+  }
+  const [words, setWords] = useState([], setTranslateWord);
 
   const setWordsList = (wordsList) => {
-    const arr = [];
-    wordsList.forEach((item) => {
-      arr.push({
+    const arr = wordsList.map((item) => {
+      return {
         id: item._id,
         word: item.word,
         wordTranslate: item.wordTranslate,
         countRight: 0,
-      });
+      };
     });
-    console.log(arr);
     setWords(arr);
+    // console.log(words);
+    // setTranslateWord();
   };
 
   useEffect(() => {
@@ -39,19 +58,60 @@ function SprintGame({ level }) {
     } else {
       fetchUnauthorizedWords(level, getRandomNum(0, 30), setWordsList);
     }
-    // setWordIndex(0);
-    console.log(words);
+    // setTranslateWord();
   }, []);
+
+  const checkAnswer = (ans) => {
+    if (ans === 'yes') {
+      if (translate === words[wordIndex].wordTranslate) {
+        if (words[wordIndex].countRight !== 3) {
+          words[wordIndex].countRight++;
+          console.log('yes');
+        }
+      } else {
+        words[wordIndex].countRight = 0;
+        console.log('no');
+      }
+    } else if (translate !== words[wordIndex].wordTranslate) {
+      if (words[wordIndex].countRight !== 3) {
+        words[wordIndex].countRight++;
+        console.log('yes');
+      }
+    } else {
+      words[wordIndex].countRight = 0;
+      console.log('no');
+    }
+    if (wordIndex < words.length) {
+      setWordIndex(wordIndex + 1);
+    } else {
+      setWordIndex(0);
+    }
+    setTranslateWord();
+  };
 
   return (
     <div className={cl.sprintGame}>
       <div className={cl.wordWrapper}>
         <span>{words[wordIndex]?.word}</span>
-        <span>{words[wordIndex]?.wordTranslate}</span>
+        <span>{translate}</span>
       </div>
       <div className={cl.btnWrapper}>
-        <button className={`${cl.answBtn} ${cl.btnNo}`}>No</button>
-        <button className={`${cl.answBtn} ${cl.btnOk}`}>Ok</button>
+        <button
+          className={`${cl.answBtn} ${cl.btnNo}`}
+          onClick={() => {
+            checkAnswer('no');
+          }}
+        >
+          No
+        </button>
+        <button
+          className={`${cl.answBtn} ${cl.btnOk}`}
+          onClick={() => {
+            checkAnswer('yes');
+          }}
+        >
+          Yes
+        </button>
       </div>
     </div>
   );
